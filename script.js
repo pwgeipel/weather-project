29320be27cd05630d76466a843cdf90ae52b577b
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-10-09 20:45:56 -0500

    js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// const kelvin = 273;
-// const key = "5589fef8dc103aa57522ba5e43e43ac8";
+// var form = document.querySelector('form')
+// var location = document.querySelector('location')
 
-// function getWeather(latitude, longitude) {
-//     let api = `https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid={key}`;
+// function searchWeather(event) {
+//     event.preventDefault()
 
-//     fetch(api)
-//         .then(function(response) {
-//             console.log(response)
-//         })}
+//     console.log('search weather...')
+// }
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={5589fef8dc103aa57522ba5e43e43ac8}`)
-    .then(function(response) {
-        console.log(response)
-    })
+
+// fetch(`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=5589fef8dc103aa57522ba5e43e43ac8`)
+//     .then(function(response) {
+//         console.log(response)
+//     })
+
+// form.addEventListener('submit', searchWeather)
