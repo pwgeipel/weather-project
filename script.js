ff6018f00046e5d45477823f5dbea9d2301b0ade
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-10-10 16:05:20 -0500

    fetch data

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-// var form = document.querySelector('form')
-// var location = document.querySelector('location')
+var form = document.querySelector('form')
+var city = document.querySelector('#city')
+let lat;
+let lon;
+const key = "5589fef8dc103aa57522ba5e43e43ac8";
+let cityEl = document.getElementById("city");
 
-// function searchWeather(event) {
-//     event.preventDefault()
+function fetchCityData(city) {
+    fetch('https://api.openweather.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + key)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+    lon = data.coord.lon;
+    lat = data.coord.lat
+}
 
-//     console.log('search weather...')
-// }
+
+document.querySelector('button').addEventListener("click", function(e)) {
+    let cityValue = cityEl.value
+    e.preventDefault();
+
+    fetchCityData(cityValue);
+}
+
 
 
 // fetch(`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=5589fef8dc103aa57522ba5e43e43ac8`)
@@ -13,4 +28,4 @@
 //         console.log(response)
 //     })
 
-// form.addEventListener('submit', searchWeather)
+form.addEventListener('submit', searchWeather)
