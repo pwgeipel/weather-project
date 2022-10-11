fce560a73d384561646e25278c2cac18e230d520
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-10-11 17:43:18 -0500

    fetches

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
 var form = document.querySelector('form')
-var city = document.querySelector('#city')
+var cityInput = document.getElementById('city')
 let lat;
 let lon;
 const key = "5589fef8dc103aa57522ba5e43e43ac8";
-let cityEl = document.getElementById("city");
+// let cityEl = document.getElementById("city");
 
-function fetchCityData(city) {
-    fetch('https://api.openweather.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + key)
-    .then((response) => console.log(response))
-//     .then((data) => {
-//         console.log(data);
-//         lon = data.coord.lon;
-//         lat = data.coord.lat
-// })        
-}
 
-function fetchCityWeather() {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + key)
-    .then(function(data) {
-        //create elements
-        //modify elements
-        //append to DOM
+function searchCity(event) {
+    event.preventDefault()
+    var cityName = cityInput.value
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + key)
+    .then(function(response) {
+        if (response.status === 200) {
+            return response.json()           
+        } else if (response.status === 404) {
+
+        }
     })
-}
+    .then(function(data) {
+        lat = data.coord.lat
+        lon = data.coord.lon
+        console.log(data)
+    
+    // fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + key)
+    // .then(function(response) {
+    //     console.log(response)
+    //     })
+    // })
+// })        
+    // })
+    
+    
 
 
-document.querySelector('button').addEventListener("click", function(e) {
-    let cityValue = cityEl.value
-    e.preventDefault();
-
-    fetchCityData(cityValue);
 })
+
+
+
+// function fetchCityWeather() {
+//     fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + key)
+//     .then(function(data) {
+//         //create elements
+//         //modify elements
+//         //append to DOM
+//     })
+// }
+
+form.addEventListener('submit', searchCity)
+
+// document.querySelector('button').addEventListener("click", function(e) {
+//     let cityValue = cityEl.value
+//     e.preventDefault();
+
+//     fetchCityData(cityValue);
+// })
 
 
 
 //set cityEl to localStorage
 //get item from localStorage
-    //append to pastSearches
+    // append to pastSearches
