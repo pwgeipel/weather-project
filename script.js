af2b1934bce7ff0113e30e9e8f2591980a236ae4
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-10-13 00:20:49 -0500

    create and append children

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function removeCards() {
 
 function resetInput() {
     cityInput.value = '';
-    cityInput.setAttribute('placeholder', "Enter a location")
+    cityInput.setAttribute('placeholder', "Enter a location");
 }
 
 function searchCity(event) {
@@ -41,7 +41,7 @@ function fetchCityData(cityName) {
     .then(function(data) {
         lat = data.coord.lat
         lon = data.coord.lon
-        console.log(data)
+        cityData = data;
 
         fetchCityLocation(lon, lat);
         return cityData;
@@ -66,16 +66,62 @@ function fetchCityLocation(lat, lon) {
             createCard(forecastData);
         } else {
             // errormessage
-        }
-        
-        
-    }) 
-    
+        }        
+    })     
 }        
     
-    
-    
+function createCityCard(cityData) {
+    const currentCard = document.createElement('div');
+    currentCard.classList.add('card', 'p-3', 'text-center');
 
+    let currentName = document.createElement('h3');
+    currentName.innerText = cityData.name;
+
+    let currentDate = document.createElement('h4');
+    currentDate.innerText = moment.unix(cityData.dt).format('dddd, MMMM DD, YYYY');
+
+    let currentTemp = document.createElement('h4');
+    currentTemp.innerText = cityData.main.temp + '° F';
+
+    let currentWind = document.createElement('p');
+    currentWind.innerText = 'Wind Speed: ' + cityData.wind.speed + ' mph';
+
+    let currentHumidity = document.createElement('p');
+    currentHumidity.innerText = 'Humidity: ' + cityData.main.humidity + '%';
+
+    currentCard.appendChild(currentName);
+    currentCard.appendChild(currentDate);
+    currentCard.appendChild(currentTemp);
+    currentCard.appendChild(currentWind);
+    currentCard.appendChild(currentHumidity);
+    currentEl.appendChild(currentCard);
+};    
+    
+function createCard(forecastData) {
+    for (var i = 5; i < forecastData.list.length; i+=8) {
+        const forecastCard = document.createElement('div');
+        forecastCard.classList.add('card', 'text-center', 'px-3');
+
+        const forecastDate = document.createElement('h5');
+        forecastDate.innerText = moment.unix(forecastData.list[i].dt).format('dddd, MMMMM DD');
+
+        const forecastTemp = document.createElement('p');
+        forecastTemp.innerText = forecastData.list[i].main.temp + '° F';
+
+        const forecastWind = document.createElement('p');
+        forecastWind.innerText = 'Wind Speed: ' + forecastData.list[i].wind.speed + ' mph';
+
+        const forecastHumidity = document.createElement('p');
+        forecastHumidity.innerText = 'Humidty: ' + forecastData.list[i].main.humidty + '%';
+
+        forecastCard.appendChild(forecastDate);
+        forecastCard.appendChild(forecastTemp);
+        forecastCard.appendChild(forecastWind);
+        forecastCard.appendChild(forecastHumidity);
+        forecastEl.appendChild(forecastCard);
+
+    }
+}
 
 
 
